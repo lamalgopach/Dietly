248a10ffa7f1c6dc232d8001b796aaa17261d94b
James Hay <jaahay@users.noreply.github.com>
2018-08-24 16:24:05 -0700

    Update server.py

--- server.py
+++ server.py
@@ -13,8 +13,11 @@ from sys import argv
 from pprint import pprint
 import json
 
+from blueprints.options_page import options_page
+
 app = Flask(__name__)
 app.secret_key = "SECRETSECRETSECRET"
+app.register_blueprint(options_page)
 
 EDAMAM_RECIPE_SEARCH_APPLICATION_ID = os.environ.get('EDAMAM_RECIPE_SEARCH_APPLICATION_ID')
 EDAMAM_RECIPE_SEARCH_APPLICATION_KEY = os.environ.get('EDAMAM_RECIPE_SEARCH_APPLICATION_KEY')
@@ -110,61 +113,6 @@ def handle_allergy_form():
 
 	return redirect("/options")
 	# make the user page more personal
-
-@app.route("/options")
-def show_options_form():
-	"""Redirect from the preferences form. Display checkbox with diet options."""
-
-	return render_template("options_diet.html")
-
-@app.route("/options", methods=["POST"])
-def handle_options_form():
-	"""Handle user's diet options."""
-	
-	diet_options = []
-
-	alcohol_free = request.form.get("option1")
-	diet_options.append(alcohol_free)
-
-	peanut_free = request.form.get("option2")
-	diet_options.append(peanut_free)
-
-	sugar_conscious = request.form.get("option3")
-	diet_options.append(sugar_conscious)
-
-	tree_nut_free = request.form.get("option4")
-	diet_options.append(tree_nut_free)
-
-	# vegan = request.form.get("option5")
-	# diet_options.append(vegan)
-
-	# vegetarian = request.form.get("option6")
-	# diet_options.append(vegetarian)
-
-	# high_protein = request.form.get("option7")
-	# diet_options.append(high_protein)
-
-	# low_carb = request.form.get("option8")
-	# diet_options.append(low_carb)
-
-	# low_fat = request.form.get("option9")
-	# diet_options.append(low_fat)
-
-	# balanced = request.form.get("option10")
-	# diet_options.append(balanced)
-
-	user_id = session["new_user_id"]
-
-	for i, diet_option in enumerate(diet_options):
-		if diet_option:
-			diet_id = i + 1
-			user_diet = UserDiet(user_id=user_id, diet_id=diet_id)
-			db.session.add(user_diet)
-
-	db.session.commit()
-
-	return redirect("/login")
-
 
 @app.route("/login")
 def login():
