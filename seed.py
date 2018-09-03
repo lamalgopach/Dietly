f7683b8f89fab2dd62a1f330b66b415fec23c358
lamalgopach <malg.pachocki@gmail.com>
2018-09-03 23:37:34 +0000

    Created function with the timer to populate db.

--- seed.py
+++ seed.py
@@ -4,10 +4,10 @@ import json
 import requests
 import os
 
-from model import Allergy, Diet, Recipe, RecipeIngredient, Ingredient, connect_to_db, db
+from model import Allergy, Diet, Recipe, RecipeIngredient, Ingredient, RecipeDiet, RecipeAllergy, connect_to_db, db
 from server import app
 
-from threading import Timer
+import time
 
 
 EDAMAM_RECIPE_SEARCH_APPLICATION_ID = os.environ.get('EDAMAM_RECIPE_SEARCH_APPLICATION_ID')
@@ -29,7 +29,7 @@ def get_recipe_attributes_db(name):
 
 
 	if response.ok:
-		for n in range(3):
+		for n in range(5):
 			recipe_obj = Recipe(recipe_name=data["hits"][n]["recipe"]["label"],
 								recipe_url=data["hits"][n]["recipe"]["uri"],
 								recipe_image=data["hits"][n]["recipe"]["image"],
@@ -43,6 +43,45 @@ def get_recipe_attributes_db(name):
 			db.session.commit()
 
 
+			recipe_labels_1 = data["hits"][n]["recipe"]["dietLabels"]
+			recipe_labels = data["hits"][n]["recipe"]["healthLabels"]
+
+			for rec_lab_1 in recipe_labels_1:
+				recipe_labels.append(rec_lab_1)
+
+			diet_id_lst = []
+			for recipe_label in recipe_labels:
+				label = Diet.query.filter_by(diet_name=recipe_label).first()
+				diet_id = label.diet_id
+				diet_id_lst.append(diet_id)
+
+
+
+			for diet_id in diet_id_lst:
+				diet_connection = RecipeDiet(recipe_id=recipe_obj.recipe_id, diet_id=diet_id)
+				db.session.add(diet_connection)
+
+			recipe_cautions = data["hits"][n]["recipe"]["cautions"]
+
+			cautions_id_lst = []
+
+			for caution in recipe_cautions:
+
+				caution_obj = Allergy.query.filter_by(allergy_name=caution).first()
+
+				caution_id = caution_obj.allergy_id
+				cautions_id_lst.append(caution_id)
+
+
+
+
+			for caution_id in cautions_id_lst:
+				caution_connection = RecipeAllergy(recipe_id=recipe_obj.recipe_id, allergy_id=caution_id)
+				db.session.add(caution_connection)
+
+
+
+
 			ingredients = data["hits"][n]["recipe"]["ingredientLines"]
 			
 			for i, ingredient in enumerate(ingredients):
@@ -52,9 +91,11 @@ def get_recipe_attributes_db(name):
 				recipe_ingredient_obj = RecipeIngredient(ingredient_id=ingredient_obj.ingredient_id, 
 														recipe_id=recipe_obj.recipe_id, 
 														amount=data["hits"][n]["recipe"]["ingredients"][i]["weight"])
-				db.session.add(recipe_ingredient_obj)
-				
+				db.session.add(recipe_ingredient_obj)	
+			print (recipe_cautions)
+
 		db.session.commit()
+
 
 
 def load_allergens(allergy_filename):
@@ -87,72 +128,72 @@ def load_diets(diet_filename):
 
 
 
-def get_recipe_attributes(name):
-	"""Get the recipes' attributes."""
+# def get_recipe_attributes(name):
+# 	"""Get the recipes' attributes."""
 
 
-	payload = { 'q': name,
-				'app_id': EDAMAM_RECIPE_SEARCH_APPLICATION_ID,
-				'app_key': EDAMAM_RECIPE_SEARCH_APPLICATION_KEY }
+# 	payload = { 'q': name,
+# 				'app_id': EDAMAM_RECIPE_SEARCH_APPLICATION_ID,
+# 				'app_key': EDAMAM_RECIPE_SEARCH_APPLICATION_KEY }
 	
-	response = requests.get(EDAMAM_URL, params=payload)
-	data = response.json()
-	# import pdb; pdb.set_trace()
+# 	response = requests.get(EDAMAM_URL, params=payload)
+# 	data = response.json()
+# 	# import pdb; pdb.set_trace()
 
-	set_of_recipes = []
+# 	set_of_recipes = []
 
 	
 
-	if response.ok:
-		for n in range(3):	
-			recipe = data["hits"][n]["recipe"]["label"]
-			set_of_recipes.append(recipe)
+# 	if response.ok:
+# 		for n in range(3):	
+# 			recipe = data["hits"][n]["recipe"]["label"]
+# 			set_of_recipes.append(recipe)
 
-			recipe_url = data["hits"][n]["recipe"]["url"]
-			set_of_recipes.append(recipe_url)
+# 			recipe_url = data["hits"][n]["recipe"]["url"]
+# 			set_of_recipes.append(recipe_url)
 
-			recipe_image = data["hits"][n]["recipe"]["image"]
-			set_of_recipes.append(recipe_image)
+# 			recipe_image = data["hits"][n]["recipe"]["image"]
+# 			set_of_recipes.append(recipe_image)
 
-			directions = data["hits"][n]["recipe"]["url"]
-			set_of_recipes.append(directions)
+# 			directions = data["hits"][n]["recipe"]["url"]
+# 			set_of_recipes.append(directions)
 
-			servings = data["hits"][n]["recipe"]["yield"]
-			set_of_recipes.append(servings)
+# 			servings = data["hits"][n]["recipe"]["yield"]
+# 			set_of_recipes.append(servings)
 
-			calories = (data["hits"][n]["recipe"]["calories"])
-			set_of_recipes.append(calories)
+# 			calories = (data["hits"][n]["recipe"]["calories"])
+# 			set_of_recipes.append(calories)
 			
-			carbs = data["hits"][n]["recipe"]["totalNutrients"]["CHOCDF"]["quantity"]
-			set_of_recipes.append(carbs)
+# 			carbs = data["hits"][n]["recipe"]["totalNutrients"]["CHOCDF"]["quantity"]
+# 			set_of_recipes.append(carbs)
 			
-			fat = data["hits"][n]["recipe"]["totalNutrients"]["FAT"]["quantity"]
-			set_of_recipes.append(fat)
+# 			fat = data["hits"][n]["recipe"]["totalNutrients"]["FAT"]["quantity"]
+# 			set_of_recipes.append(fat)
 			
-			protein = data["hits"][n]["recipe"]["totalNutrients"]["PROCNT"]["quantity"]			
-			set_of_recipes.append(protein)
+# 			protein = data["hits"][n]["recipe"]["totalNutrients"]["PROCNT"]["quantity"]			
+# 			set_of_recipes.append(protein)
 
-			ingredients_dict = {}
-			ingredients = data["hits"][n]["recipe"]["ingredientLines"]
-			set_of_recipes.append(ingredients_dict)
+# 			ingredients_dict = {}
+# 			ingredients = data["hits"][n]["recipe"]["ingredientLines"]
+# 			set_of_recipes.append(ingredients_dict)
 			
-			for i, ing in enumerate(ingredients):
-				ingredients_dict[ing] = data["hits"][n]["recipe"]["ingredients"][i]["weight"]
+# 			for i, ing in enumerate(ingredients):
+# 				ingredients_dict[ing] = data["hits"][n]["recipe"]["ingredients"][i]["weight"]
 
 
-	return set_of_recipes
+# 	return set_of_recipes
 
 
-def print_recipe_to_textfile(set_of_recipes):
-	"""Print the recipe's attributes to the text file"""
+# def print_recipe_to_textfile(set_of_recipes):
+# 	"""Print the recipe's attributes to the text file"""
 	
-	set_of_recipes = str(set_of_recipes)
+# 	set_of_recipes = str(set_of_recipes)
 
-	f = open("./seed_data/database.txt", "a")
-	f.write(set_of_recipes)
-	f.write("\n")
-	f.write("\n")
-	f.close()
+# 	f = open("./seed_data/database.txt", "a")
+# 	f.write(set_of_recipes)
+# 	f.write("\n")
+# 	f.write("\n")
+# 	f.close()
 
 ###########################################to do::::
 
@@ -212,8 +253,6 @@ if __name__ == "__main__":
     connect_to_db(app)
     db.create_all()
 
-    # name = "pork"
-    # set_of_recipes = get_recipe_attributes(name)
     # print_recipe_to_textfile(set_of_recipes)
 
     # database = "seed_data/database.txt"
@@ -225,29 +264,41 @@ if __name__ == "__main__":
     # name = "lunch"
     # name = "dinner"
 
-    # names = ["breakfast", "lunch", "dinner", "chicken", "pancake", 
-    # "apple", "egg", "avocado","kosher", "fish", "pork", "halal", "beef"]
+
+    
 
     # names = ["breakfast", "lunch", "dinner"]
 
-    # name = "egg"
-    # get_recipe_attributes_db(name)
 
     allergy_filename = "seed_data/allergens.txt"
     diet_filename = "seed_data/diets.txt"
     load_allergens(allergy_filename)
     load_diets(diet_filename)
 
+    meal_list = ["fish", "zucchini", "crepes", "dinner", "broccoli", "pancakes", "pork", "kebab", "taco", "tacos", "steak", "schnitzel", "gulash", "hungarian", "dumplings", "pasta"]
+ 
+    
+
+# "breakfast", "pancake", "eggs", "chicken", "beef", "avocado", "banana", "muesli", "lunch", "poke", "garlic", "pizza", - ok
 
 
-    # name = "dinner"
-    # t = Timer(90.0, get_recipe_attributes_db(name))
-    # t.start()
+# bad names: pork, protein, oatmeal "scrambled", "chinese", "apple", "smoothie", "omelette", "bruschetta", "sausage(s)", "soup", "panini",
+# "ramen",   "spaghetti", "salad", "hamburger", "pierogies",  "mozarella", 
 
-    # name = "chicken"
+    for i in meal_list:
 
-    # t = Timer(90.0, get_recipe_attributes_db(name))
-    # t.start()
+    	get_recipe_attributes_db(i)
+    	time.sleep(90.0)
+
+
+
+
+
+    
+
+
+
+
 
 
 
